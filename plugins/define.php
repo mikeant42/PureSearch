5d994ee9d1c78e12ef8ec5fa6bc2c393627315df
I. Gaffling <10500034+gaffling@users.noreply.github.com>
2019-07-08 11:08:03 +0200

    Need this changes
    Need this changes so that it works for me with PHP 7 and >

--- plugins/define.php
+++ plugins/define.php
@@ -3,23 +3,13 @@ require_once 'pluginfuncs.php';
 	function ini_define($search) {
 		if (strtok($search, " ") === 'define') {
 			$search = preg_replace('/(define)/', '', $search);
-			$dict_url = str_replace(' ', '', "http://www.thefreedictionary.com/$search");
-			$div = "ds-list";
-			
-			$config = array(
-			  'indent' => 'true',
-			  'output-xhtml' => 'true',
-			  'wrap          => 200'
-			);
-			@$tidy = tidy_parse_string(file_get_contents($dict_url), $config, 'utf8');
-			$tidy->cleanRepair();
-			
-			$doc = new DomDocument;
-			@$doc->loadHTML($tidy);
-			@$definition = getElementsByClassName($doc, $div);
-			if ($definition == "") {
-				echo '';
-			} else {
+			$html = file_get_contents($dict_url);
+			$dom = new DOMDocument();
+			@$dom->loadHTML($html); // Show no errors on missformed HTML ;-)
+			$xpath = new DOMXpath($dom);
+			$note = $xpath->query('//div[contains(@class, "ds-list")]');
+			$definition = str_replace('1. ', '', trim(@$note->item(0)->nodeValue));
+			if ($definition != '') {
 				echo space() . "<div class='definition-box'><span class='definition'>The definition of <strong>$search</strong> <br><ul><em>$definition</em></ul>
 				Read more at <a href='http://www.thefreedictionary.com/$search'>The Free Dictionary</a></span></div>";
 			}
